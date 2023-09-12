bca4de473b742bb09a8d76e46e81f2bcbce40fe3
Zeyad Mohamed Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2023-09-12 21:04:20 +0300

    init EXTI8 and 9 static void Tetris_moveBlockRight() create moveBlock

--- src/EXTI_Program.c
+++ src/EXTI_Program.c
@@ -13,23 +13,39 @@
 #include "MCAL/NVIC/NVIC_Interface.h"
 #include "MCAL/GPIO/GPIO_Interface.h"
 
-
 static EXTI_Callback_t EXTI_Callbacks[EXTI_MAX_LINE_NUMBERS];
 
-void EXTI_voidInit(u8 copy_u8LineID) {
+void EXTI_voidInit(u8 copy_u8LineID)
+{
 
-	switch (copy_u8LineID) {
-	case EXTI9:
+	switch (copy_u8LineID)
+	{
+	case EXTI8:
+		GPIO_voidSetPinMode(IOA, PIN8, INPUT);
+		GPIO_voidSetPinPuPdConfig(IOA, PIN8, INPUT_PU);
+		GPIO_voidSetPinSpeed(IOA, PIN8, OUTPUT_LS);
+		EXTI_voidEXTILineEnable(EXTI8);
+		EXTI_voidSetSenseControl(EXTI8, EXTI_SC_FallingEdge);
 		NVIC_u8EnablePerInt(23);
+		break;
 
+	case EXTI9:
 		GPIO_voidSetPinMode(IOA, PIN9, INPUT);
 		GPIO_voidSetPinPuPdConfig(IOA, PIN9, INPUT_PU);
 		GPIO_voidSetPinSpeed(IOA, PIN9, OUTPUT_LS);
 		EXTI_voidEXTILineEnable(EXTI9);
 		EXTI_voidSetSenseControl(EXTI9, EXTI_SC_FallingEdge);
+		NVIC_u8EnablePerInt(23);
+		break;
+	case EXTI10:
+		GPIO_voidSetPinMode(IOA, PIN10, INPUT);
+		GPIO_voidSetPinPuPdConfig(IOA, PIN10, INPUT_PU);
+		GPIO_voidSetPinSpeed(IOA, PIN10, OUTPUT_LS);
+		EXTI_voidEXTILineEnable(EXTI10);
+		EXTI_voidSetSenseControl(EXTI10, EXTI_SC_FallingEdge);
+		NVIC_u8EnablePerInt(23);
 		break;
 	}
-
 }
 
 /*
@@ -38,8 +54,10 @@ void EXTI_voidInit(u8 copy_u8LineID) {
  o/p arguments: void
  Description: this API allows the user to enable the EXTI Interrupt Line
  */
-void EXTI_voidEXTILineEnable(u8 copy_u8LineID) {
-	if (copy_u8LineID < 22 && copy_u8LineID != 20 && copy_u8LineID != 19) {
+void EXTI_voidEXTILineEnable(u8 copy_u8LineID)
+{
+	if (copy_u8LineID < 22 && copy_u8LineID != 20 && copy_u8LineID != 19)
+	{
 		SET_BIT(EXTI->IMR, copy_u8LineID);
 	}
 }
@@ -50,14 +68,17 @@ void EXTI_voidEXTILineEnable(u8 copy_u8LineID) {
  o/p arguments: void
  Description: this API allows the user to disable the EXTI Interrupt Line
  */
-void EXTI_voidEXTILineDisable(u8 copy_u8LineID) {
-	if (copy_u8LineID < 22 && copy_u8LineID != 20 && copy_u8LineID != 19) {
+void EXTI_voidEXTILineDisable(u8 copy_u8LineID)
+{
+	if (copy_u8LineID < 22 && copy_u8LineID != 20 && copy_u8LineID != 19)
+	{
 		CLR_BIT(EXTI->IMR, copy_u8LineID);
 	}
 }
-void EXTI_voidClrPendingFlag(u8 copy_u8EXTILineId) {
-	if (copy_u8EXTILineId < 22 && copy_u8EXTILineId != 20
-			&& copy_u8EXTILineId != 19) {
+void EXTI_voidClrPendingFlag(u8 copy_u8EXTILineId)
+{
+	if (copy_u8EXTILineId < 22 && copy_u8EXTILineId != 20 && copy_u8EXTILineId != 19)
+	{
 		SET_BIT(EXTI->PR, copy_u8EXTILineId);
 	}
 }
@@ -68,19 +89,26 @@ void EXTI_voidClrPendingFlag(u8 copy_u8EXTILineId) {
  o/p arguments: void
  Description: this API allows the user to Set Sense control for a given Line
  */
-void EXTI_voidSetSenseControl(u8 copy_u8Line, u8 copy_u8SenseControl) {
-	if (copy_u8Line > EXTI15) {
+void EXTI_voidSetSenseControl(u8 copy_u8Line, u8 copy_u8SenseControl)
+{
+	if (copy_u8Line > EXTI15)
+	{
 		// invalid EXTI Line Id
 		return;
 	}
 
-	if (copy_u8SenseControl == EXTI_SC_DoubleEdge) {
+	if (copy_u8SenseControl == EXTI_SC_DoubleEdge)
+	{
 		SET_BIT(EXTI->RTSR, copy_u8Line);
 		SET_BIT(EXTI->FTSR, copy_u8Line);
-	} else if (copy_u8SenseControl == EXTI_SC_RisingEdge) {
+	}
+	else if (copy_u8SenseControl == EXTI_SC_RisingEdge)
+	{
 		SET_BIT(EXTI->RTSR, copy_u8Line);
 		CLR_BIT(EXTI->FTSR, copy_u8Line);
-	} else if (copy_u8SenseControl == EXTI_SC_FallingEdge) {
+	}
+	else if (copy_u8SenseControl == EXTI_SC_FallingEdge)
+	{
 		CLR_BIT(EXTI->RTSR, copy_u8Line);
 		SET_BIT(EXTI->FTSR, copy_u8Line);
 	}
@@ -95,149 +123,190 @@ void EXTI_voidSetSenseControl(u8 copy_u8Line, u8 copy_u8SenseControl) {
  o/p arguments: void
  Description: this API allows the user to Set Sense control for a given Line
  */
-void EXTI_voidSetCallBack(EXTI_Callback_t callback, u8 copy_u8LineId) {
+void EXTI_voidSetCallBack(EXTI_Callback_t callback, u8 copy_u8LineId)
+{
 
-	if (copy_u8LineId <= 15) {
+	if (copy_u8LineId <= 15)
+	{
 		EXTI_Callbacks[copy_u8LineId] = callback;
 	}
 }
 
-void EXTI0_IRQHandler(void) {
+void EXTI0_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI0) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI0) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI0);
-		if (EXTI_Callbacks[private_EXTI0] != NULL) {
+		if (EXTI_Callbacks[private_EXTI0] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI0]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI0);
 }
-void EXTI1_IRQHandler(void) {
+void EXTI1_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI1) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI1) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI1);
-		if (EXTI_Callbacks[private_EXTI1] != NULL) {
+		if (EXTI_Callbacks[private_EXTI1] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI1]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI1);
 }
-void EXTI2_IRQHandler(void) {
+void EXTI2_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI2) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI2) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI2);
-		if (EXTI_Callbacks[private_EXTI2] != NULL) {
+		if (EXTI_Callbacks[private_EXTI2] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI2]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI2);
 }
-void EXTI3_IRQHandler(void) {
+void EXTI3_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI3) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI3) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI3);
-		if (EXTI_Callbacks[private_EXTI3] != NULL) {
+		if (EXTI_Callbacks[private_EXTI3] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI3]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI3);
 }
-void EXTI4_IRQHandler(void) {
+void EXTI4_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI4) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI4) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI4);
-		if (EXTI_Callbacks[private_EXTI4] != NULL) {
+		if (EXTI_Callbacks[private_EXTI4] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI4]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI4);
 }
-void EXTI9_5_IRQHandler(void) {
+void EXTI9_5_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI5) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI5) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI5);
-		if (EXTI_Callbacks[private_EXTI5] != NULL) {
+		if (EXTI_Callbacks[private_EXTI5] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI5]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI6) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI6) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI6);
-		if (EXTI_Callbacks[private_EXTI6] != NULL) {
+		if (EXTI_Callbacks[private_EXTI6] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI6]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI7) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI7) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI7);
-		if (EXTI_Callbacks[private_EXTI7] != NULL) {
+		if (EXTI_Callbacks[private_EXTI7] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI7]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI8) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI8) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI8);
-		if (EXTI_Callbacks[private_EXTI8] != NULL) {
+		if (EXTI_Callbacks[private_EXTI8] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI8]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI9) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI9) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI9);
-		if (EXTI_Callbacks[private_EXTI9] != NULL) {
+		if (EXTI_Callbacks[private_EXTI9] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI9]();
 		}
 	}
 	EXTI_voidClrPendingFlag(EXTI9);
 }
 
-void EXTI10_15_IRQHandler(void) {
+void EXTI10_15_IRQHandler(void)
+{
 
 	// check if the pending flag is on
-	if (GET_BIT(EXTI->PR, private_EXTI10) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI10) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI10);
-		if (EXTI_Callbacks[private_EXTI10] != NULL) {
+		if (EXTI_Callbacks[private_EXTI10] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI10]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI11) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI11) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI11);
-		if (EXTI_Callbacks[private_EXTI11] != NULL) {
+		if (EXTI_Callbacks[private_EXTI11] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI11]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI12) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI12) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI12);
-		if (EXTI_Callbacks[private_EXTI12] != NULL) {
+		if (EXTI_Callbacks[private_EXTI12] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI12]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI13) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI13) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI13);
-		if (EXTI_Callbacks[private_EXTI13] != NULL) {
+		if (EXTI_Callbacks[private_EXTI13] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI13]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI14) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI14) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI14);
-		if (EXTI_Callbacks[private_EXTI14] != NULL) {
+		if (EXTI_Callbacks[private_EXTI14] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI14]();
 		}
 	}
 
-	if (GET_BIT(EXTI->PR, private_EXTI15) == 1) {
+	if (GET_BIT(EXTI->PR, private_EXTI15) == 1)
+	{
 		SET_BIT(EXTI->PR, private_EXTI15);
-		if (EXTI_Callbacks[private_EXTI15] != NULL) {
+		if (EXTI_Callbacks[private_EXTI15] != NULL)
+		{
 			EXTI_Callbacks[private_EXTI15]();
 		}
 	}
--- src/Tetris.c
+++ src/Tetris.c
@@ -36,7 +36,11 @@ static void Tetris_remove_L();
 static void Tetris_remove_S();
 static void Tetris_remove_Z();
 
-s8 Tetris_drawShape(Tetris *board, u8 copy_u8X, u8 copy_u8Y) {
+s8 Tetris_moveBlockRight();
+s8 Tetris_moveBlockLeft();
+
+s8 Tetris_drawShape(Tetris *board, u8 copy_u8X, u8 copy_u8Y)
+{
 	//    s8 LOC_s8BlockNum = (rand() % TETRIS_SHAPE_COUNT);
 	// static u8 shape=3;
 
@@ -54,7 +58,8 @@ s8 Tetris_drawShape(Tetris *board, u8 copy_u8X, u8 copy_u8Y) {
 	board->active_block = new_block;
 
 	s8 LOC_u8ErrCode = 0;
-	switch (new_block.type) {
+	switch (new_block.type)
+	{
 	case TETRIS_SHAPE_I:
 		LOC_u8ErrCode = Tetris_draw_I(&(board->board), new_block);
 		break;
@@ -85,36 +90,38 @@ s8 Tetris_drawShape(Tetris *board, u8 copy_u8X, u8 copy_u8Y) {
 	return LOC_s8BlockNum;
 }
 
-static s8 Tetris_drawShape2(void) {
+static s8 Tetris_drawShape2(void)
+{
 	s8 LOC_u8ErrCode = TRUE;
-	switch (game_controller.active_block.type) {
+	switch (game_controller.active_block.type)
+	{
 	case TETRIS_SHAPE_I:
 		LOC_u8ErrCode = Tetris_draw_I(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_O:
 		LOC_u8ErrCode = Tetris_draw_O(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_T:
 		LOC_u8ErrCode = Tetris_draw_T(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_J:
 		LOC_u8ErrCode = Tetris_draw_J(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_L:
 		LOC_u8ErrCode = Tetris_draw_L(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_S:
 		LOC_u8ErrCode = Tetris_draw_S(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 	case TETRIS_SHAPE_Z:
 		LOC_u8ErrCode = Tetris_draw_Z(&(game_controller.board),
-				game_controller.active_block);
+									  game_controller.active_block);
 		break;
 
 	default:
@@ -123,65 +130,79 @@ static s8 Tetris_drawShape2(void) {
 	return LOC_u8ErrCode;
 }
 
-static s8 Tetris_draw_I(DotMatrix *board, const Block new_block) {
+static s8 Tetris_draw_I(DotMatrix *board, const Block new_block)
+{
 	u8 LOC_u8Center_x = new_block.center_x;
 	u8 LOC_u8Center_y = new_block.center_y;
 	s8 LOC_s8DrawStatus = TRUE;
 
 	u8 movements = 0;
-	switch (new_block.rotation) {
+	switch (new_block.rotation)
+	{
 	case TETRIS_ROTATION_0:
-		for (; movements < MAX_PIXELS_PER_BLOCK - 1; movements++) {
-			LOC_s8DrawStatus = board->movePixelDown(board->buffer,LOC_u8Center_x--, LOC_u8Center_y, ON_MOVE_SET_OLD);
+		for (; movements < MAX_PIXELS_PER_BLOCK - 1; movements++)
+		{
+			LOC_s8DrawStatus = board->movePixelDown(board->buffer, LOC_u8Center_x--, LOC_u8Center_y, ON_MOVE_SET_OLD);
 		}
-		if (LOC_s8DrawStatus != TRUE) {
+		if (LOC_s8DrawStatus != TRUE)
+		{
 			if (LOC_s8DrawStatus == OutOfBoundsException)
 				LOC_u8Center_x = 0;
-			for (u8 reverse = 0; reverse < movements; reverse++) {
+			for (u8 reverse = 0; reverse < movements; reverse++)
+			{
 				board->clrPixel(board->buffer, LOC_u8Center_x++,
-						LOC_u8Center_y);
+								LOC_u8Center_y);
 			}
 		}
 		break;
 	case TETRIS_ROTATION_90:
-		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++) {
+		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++)
+		{
 			LOC_s8DrawStatus = board->movePixelLeft(board->buffer,
-					LOC_u8Center_x, LOC_u8Center_y--, ON_MOVE_SET_OLD);
+													LOC_u8Center_x, LOC_u8Center_y--, ON_MOVE_SET_OLD);
 		}
-		if (LOC_s8DrawStatus != TRUE) {
+		if (LOC_s8DrawStatus != TRUE)
+		{
 			if (LOC_s8DrawStatus == OutOfBoundsException)
 				LOC_u8Center_y = 0;
-			for (u8 reverse = 0; reverse < movements; reverse++) {
+			for (u8 reverse = 0; reverse < movements; reverse++)
+			{
 				board->clrPixel(board->buffer, LOC_u8Center_x,
-						LOC_u8Center_y++);
+								LOC_u8Center_y++);
 			}
 		}
 		break;
 	case TETRIS_ROTATION_180:
-		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++) {
+		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++)
+		{
 			LOC_s8DrawStatus = board->movePixelUp(board->buffer,
-					LOC_u8Center_x++, LOC_u8Center_y, ON_MOVE_SET_OLD);
+												  LOC_u8Center_x++, LOC_u8Center_y, ON_MOVE_SET_OLD);
 		}
-		if (LOC_s8DrawStatus != TRUE) {
+		if (LOC_s8DrawStatus != TRUE)
+		{
 			if (LOC_s8DrawStatus == OutOfBoundsException)
 				LOC_u8Center_x = 0;
-			for (u8 reverse = 0; reverse < movements; reverse++) {
+			for (u8 reverse = 0; reverse < movements; reverse++)
+			{
 				board->clrPixel(board->buffer, LOC_u8Center_x--,
-						LOC_u8Center_y);
+								LOC_u8Center_y);
 			}
 		}
 		break;
 	case TETRIS_ROTATION_270:
-		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++) {
+		for (movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1; movements++)
+		{
 			LOC_s8DrawStatus = board->movePixelRight(board->buffer,
-					LOC_u8Center_x, LOC_u8Center_y++, ON_MOVE_SET_OLD);
+													 LOC_u8Center_x, LOC_u8Center_y++, ON_MOVE_SET_OLD);
 		}
-		if (LOC_s8DrawStatus != TRUE) {
+		if (LOC_s8DrawStatus != TRUE)
+		{
 			if (LOC_s8DrawStatus == OutOfBoundsException)
 				LOC_u8Center_y = new_block.center_y;
-			for (u8 reverse = 0; reverse < movements; reverse++) {
+			for (u8 reverse = 0; reverse < movements; reverse++)
+			{
 				board->clrPixel(board->buffer, LOC_u8Center_x,
-						LOC_u8Center_y++);
+								LOC_u8Center_y++);
 			}
 		}
 		break;
@@ -191,122 +212,143 @@ static s8 Tetris_draw_I(DotMatrix *board, const Block new_block) {
 	}
 	return LOC_s8DrawStatus;
 }
-static s8 Tetris_draw_O(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_O(DotMatrix *board, Block new_block)
+{
 	u8 LOC_u8Center_x = new_block.center_x;
 	u8 LOC_u8Center_y = new_block.center_y;
 	s8 LOC_u8DrawStatus = TRUE;
 
 	LOC_u8DrawStatus = board->movePixelRight(board->buffer, LOC_u8Center_x,
-			LOC_u8Center_y++, ON_MOVE_SET_OLD);
-	if (LOC_u8DrawStatus != TRUE) {
+											 LOC_u8Center_y++, ON_MOVE_SET_OLD);
+	if (LOC_u8DrawStatus != TRUE)
+	{
 		board->movePixelLeft(board->buffer, LOC_u8Center_x, LOC_u8Center_y,
-				ON_MOVE_SET_OLD);
+							 ON_MOVE_SET_OLD);
 	}
 
 	return LOC_u8DrawStatus;
 }
-static s8 Tetris_draw_T(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_T(DotMatrix *board, Block new_block)
+{
 	s8 LOC_u8DrawStatus = TRUE;
 
 	LOC_u8DrawStatus = board->movePixelLeft(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
+											new_block.center_y, ON_MOVE_SET_OLD);
 	LOC_u8DrawStatus = board->movePixelDown(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
+											new_block.center_y, ON_MOVE_SET_OLD);
 	LOC_u8DrawStatus = board->movePixelRight(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
+											 new_block.center_y, ON_MOVE_SET_OLD);
 
 	return LOC_u8DrawStatus;
 }
-static s8 Tetris_draw_J(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_J(DotMatrix *board, Block new_block)
+{
 	s8 LOC_u8DrawStatus = TRUE;
 
 	LOC_u8DrawStatus = board->movePixelLeft(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
-	if (LOC_u8DrawStatus != TRUE) {
+											new_block.center_y, ON_MOVE_SET_OLD);
+	if (LOC_u8DrawStatus != TRUE)
+	{
 		return LOC_u8DrawStatus;
 	}
 
 	LOC_u8DrawStatus = board->movePixelUp(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
-	if (LOC_u8DrawStatus != TRUE) {
+										  new_block.center_y, ON_MOVE_SET_OLD);
+	if (LOC_u8DrawStatus != TRUE)
+	{
 		board->clrPixel(board->buffer, new_block.center_x, new_block.center_y);
 		board->clrPixel(board->buffer, new_block.center_x,
-				new_block.center_y + 1);
+						new_block.center_y + 1);
 		return LOC_u8DrawStatus;
 	}
 	new_block.center_x++;
 
 	LOC_u8DrawStatus = board->movePixelUp(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
-	if (LOC_u8DrawStatus != TRUE) {
+										  new_block.center_y, ON_MOVE_SET_OLD);
+	if (LOC_u8DrawStatus != TRUE)
+	{
 		board->clrPixel(board->buffer, new_block.center_x++,
-				new_block.center_y);
+						new_block.center_y);
 		board->clrPixel(board->buffer, new_block.center_x, new_block.center_y);
 		board->clrPixel(board->buffer, new_block.center_x,
-				new_block.center_y + 1);
+						new_block.center_y + 1);
 		return LOC_u8DrawStatus;
 	}
 
 	return LOC_u8DrawStatus;
 }
-static s8 Tetris_draw_L(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_L(DotMatrix *board, Block new_block)
+{
 	s8 LOC_s8DrawStatus = TRUE;
 	LOC_s8DrawStatus = board->movePixelRight(board->buffer, new_block.center_x,
-			new_block.center_y, ON_MOVE_SET_OLD);
+											 new_block.center_y, ON_MOVE_SET_OLD);
 	LOC_s8DrawStatus = board->movePixelUp(board->buffer, new_block.center_x++,
-			new_block.center_y, ON_MOVE_SET_OLD);
+										  new_block.center_y, ON_MOVE_SET_OLD);
 	LOC_s8DrawStatus = board->movePixelUp(board->buffer, new_block.center_x++,
-			new_block.center_y, ON_MOVE_SET_OLD);
+										  new_block.center_y, ON_MOVE_SET_OLD);
 
 	return LOC_s8DrawStatus;
 }
-static s8 Tetris_draw_S(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_S(DotMatrix *board, Block new_block)
+{
 }
-static s8 Tetris_draw_Z(DotMatrix *board, Block new_block) {
+static s8 Tetris_draw_Z(DotMatrix *board, Block new_block)
+{
 }
 
-Tetris Tetris_init(void) {
+Tetris Tetris_init(void)
+{
 	Tetris game_controller;
 	game_controller.board = DotMatrix_init();
 	game_controller.drawShape = Tetris_drawShape;
+	EXTI_voidInit(EXTI8);
 	EXTI_voidInit(EXTI9);
+	EXTI_voidInit(EXTI10);
 	NVIC_voidSetPriorityConfig(G0_SG16);
 	EXTI_voidSetCallBack(Tetris_rotate, EXTI9);
+	EXTI_voidSetCallBack(Tetris_moveBlockRight, EXTI9);
+	EXTI_voidSetCallBack(Tetris_moveBlockLeft, EXTI9);
 	return game_controller;
 }
 
-static void Tetris_remove_I() {
+static void Tetris_remove_I()
+{
 	Block new_block = game_controller.active_block;
 	DotMatrix *board = &game_controller.board;
 
 	u8 LOC_u8Center_x = new_block.center_x;
 	u8 LOC_u8Center_y = new_block.center_y;
 
-	switch (new_block.rotation) {
+	switch (new_block.rotation)
+	{
 	case TETRIS_ROTATION_0:
 		for (u8 movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1;
-				movements++) {
+			 movements++)
+		{
 			board->clrPixel(board->buffer, LOC_u8Center_x--, LOC_u8Center_y);
 		}
 		board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y);
 		break;
 	case TETRIS_ROTATION_90:
 		for (u8 movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1;
-				movements++) {
+			 movements++)
+		{
 			board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y--);
 		}
 		board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y);
 		break;
 	case TETRIS_ROTATION_180:
 		for (u8 movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1;
-				movements++) {
+			 movements++)
+		{
 			board->clrPixel(board->buffer, LOC_u8Center_x++, LOC_u8Center_y);
 		}
 		board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y);
 		break;
 	case TETRIS_ROTATION_270:
 		for (u8 movements = 0; movements < MAX_PIXELS_PER_BLOCK - 1;
-				movements++) {
+			 movements++)
+		{
 			board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y++);
 		}
 		board->clrPixel(board->buffer, LOC_u8Center_x, LOC_u8Center_y);
@@ -317,9 +359,11 @@ static void Tetris_remove_I() {
 	}
 }
 
-Tetris_removeActiveBlock() {
+Tetris_removeActiveBlock()
+{
 
-	switch (game_controller.active_block.type) {
+	switch (game_controller.active_block.type)
+	{
 	case TETRIS_SHAPE_I:
 		Tetris_remove_I();
 		break;
@@ -347,21 +391,30 @@ Tetris_removeActiveBlock() {
 	}
 }
 
-s8 Tetris_rotate(void) {
+s8 Tetris_rotate(void)
+{
 	s8 LOC_u8RotationErr = 1;
 
 	Tetris_removeActiveBlock(); // remove the current block to prevent it from colliding with itself
 
 	game_controller.active_block.rotation =
-			(game_controller.active_block.rotation + 1) % 4; // there are only 4 rotations:   0� -> 360�
+		(game_controller.active_block.rotation + 1) % 4; // there are only 4 rotations:   0� -> 360�
 	LOC_u8RotationErr = Tetris_drawShape2();
 
 	// if there was an error rotating, draw the block with its old rotation.
-	if (LOC_u8RotationErr != TRUE) {
+	if (LOC_u8RotationErr != TRUE)
+	{
 		game_controller.active_block.rotation =
-				(game_controller.active_block.rotation - 1) % 4;
+			(game_controller.active_block.rotation - 1) % 4;
 		Tetris_drawShape2();
 	}
 
 	return LOC_u8RotationErr;
+}
+
+s8 Tetris_moveBlockRight()
+{
+}
+s8 Tetris_moveBlockLeft()
+{
 }
